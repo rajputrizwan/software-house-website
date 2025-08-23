f0002fff11b52a38f4143f49d3734ece3bfa6f3e
Ayaz Hussain <ayazhussain.bssef22@iba-suk.edu.pk>
2025-08-23 18:55:31 +0500

    froked and made changes

--- src/app/Services/page.jsx
+++ src/app/Services/page.jsx
@@ -1,6 +1,6 @@
 
 export default function Services (){
     return <div className="justify-center items-center m-20 ">
-        <h1>Hello AYaz Hussain </h1>
+        <h1>Hello AYaz Hussain froked and working  </h1>
     </div>
 }
