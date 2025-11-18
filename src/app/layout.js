d1609080c6aad998b99f5f7ee5e5e96afcc4b366
ayaz7964 <ayazhussain.bssef22@iba-suk.edu.pk>
2025-11-18 16:40:41 +0500

    added the google concole mets code in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,11 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
       // className="scroll-smooth"
     >
-      <head />
+      <head
+      
+      >
+        <meta name="google-site-verification" content="jnByv2oanpnLvcEMWBV4H0eoPG19XmodKZipRmVK0Nc" />
+      </head>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
